e00b67994354bf89daee79870ef24e5b30962883
Newerton Vargas de Araujo <newerton.araujo@inmetrics.com.br>
2023-12-01 00:17:21 -0300

    chore: add interface in callback param

--- src/main.service.ts
+++ src/main.service.ts
@@ -1,13 +1,18 @@
 import cluster from 'node:cluster';
+import * as os from 'node:os';
 import * as process from 'node:process';
 
 import { Injectable, Logger } from '@nestjs/common';
 
-const numCPUs = parseInt(process.argv[2] || '2');
+const numCPUs = os.cpus().length;
+
+interface CallbackFunction {
+  (): Promise<void> | void;
+}
 
 @Injectable()
 export class MainService {
-  static clusterize(callback): void {
+  static clusterize(callback: CallbackFunction): void {
     const logger = new Logger(MainService.name);
     if (cluster.isPrimary) {
       logger.verbose(`Master Server is running in process ${process.pid}`);
